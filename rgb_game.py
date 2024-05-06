e82afff1914ea6f773823936e2869d03b0d4a5c2
ChondromatidisEleftherios <148573871+ChondromatidisEleftherios@users.noreply.github.com>
2024-05-06 19:34:16 +0300

    Update rgb_game.py

--- rgb_game.py
+++ rgb_game.py
@@ -9,6 +9,22 @@ attempt = 1 # Μετρητής προσπαθειών
 rst = False
 auto_va = True # Χρησιμοποιείται για το πρώτο generation το οποίο είναι αυτόματο
 
+def cursorb1(a): # Συνάρτηση για το πρώτο κουμπί, όταν ο κέρσορας είναι πάνω του
+   button1.config(background='white', foreground= "darkorchid4")
+
+def no_cursorb1(a): # Συνάρτηση για το πρώτο κουμπί, όταν ο κέρσορας δεν είναι πάνω του
+   button1.config(background= 'cyan', foreground= 'darkorchid4')
+
+def cursorb2(a): # Συνάρτηση για το δεύτερο κουμπί, όταν ο κέρσορας είναι πάνω του
+   button2.config(background='white', foreground= "darkorchid4")
+
+def no_cursorb2(a): # Συνάρτηση για το δεύτερο κουμπί, όταν ο κέρσορας δεν είναι πάνω του
+   button2.config(background= 'cyan', foreground= 'darkorchid4')
+
+def reset_sliders(): # Συνάρτηση για να πάνε όλα τα sliders στο 0
+	red.set(0)
+	green.set(0)
+	blue.set(0)
 
 def upd(c):
 	global rst 
@@ -22,7 +38,7 @@ def upd(c):
 		pl_col = tk.Label(t_frame, height=1, width=15, fg='white', bg= 'darkorchid4', text = " ")
 		pl_col.grid(row = 0, column = 0, sticky = tk.NSEW)
 
-def random_col():
+def random_col(): # Συνάρτηση που κάνει generate ένα τυχαίο χρώμα
 	global prev_distance
 	global rst
 	global attempt
@@ -38,10 +54,11 @@ def random_col():
 	b= rdm.randint(1,254)
 	ai_code = '#%02x%02x%02x' % (r, g, b) # Mετατροπή των χρωμάτων σε 16δικο αριθμό
 	ai_frame.config (bg= ai_code)
+	reset_sliders()
 	if rst == False:
 		att = tk.Label(at_frame, height=1, width= 10, fg='red', bg='darkorchid4', text= "Attempt " + str(attempt), font= ('Cambria', 24, 'bold'))
 		att.grid(row=0, column=2, sticky = tk.NSEW)
-	if rst==True:
+	if rst==True: # Σε περίπτωση που η παρτίδα έχει τελειώσει (αποφυγή bug)
 		att = tk.Label(at_frame, height=1, width= 10, fg='red', bg='darkorchid4', text= "Attempt " + str(attempt), font= ('Cambria', 24, 'bold'))
 		att.grid(row=0, column=2, sticky = tk.NSEW)
 		pl_frame.config(bg= "black")
@@ -53,43 +70,42 @@ def random_col():
 		rst = False
 
 
-def di():
+def di(): # Συνάρτηση για υπολογισμό απόστασης, εμφάνισης καλύτερου σκορ παρτίδας και αποτελέσματος παρτίδας
 	global attempt 
 	global prev_distance
 	global rst
 	attempt= attempt + 1
 	distance = u.diff(red.get(), green.get(), blue.get(), r,g,b) # Yπολογισμός colour distance
 	if rst == False:
-		if distance <= prev_distance and attempt < 6:
+		if distance <= prev_distance and attempt < 7: # Υπολογισμός της καλύτερης απόστασης της παρτίδας
 			prev_distance = distance
 			score = tk.Label(t2_frame, height=1, width= 44, fg='white', bg='darkorchid4', text= "Best Score= " + str(distance) + "% away from the colour", font= ('Verdana', '12'))
 			score.grid(row=0, column=2, sticky = tk.NSEW)
 			att = tk.Label(at_frame, height=1, width= 10, fg='red', bg='darkorchid4', text= "Attempt " + str(attempt), font= ('Cambria', 24, 'bold'))
 			att.grid(row=0, column=2, sticky = tk.NSEW)
-		elif distance > prev_distance and attempt < 6 :
+		elif distance > prev_distance and attempt < 7 : # Υπολογισμός της καλύτερης απόστασης της παρτίδας
 			score = tk.Label(t2_frame, height=1, width= 44, fg='white', bg='darkorchid4', text= "Best Score= " + str(prev_distance) + "% away from the colour", font= ('Verdana', '12'))
 			score.grid(row=0, column=2, sticky = tk.NSEW)
 			att = tk.Label(at_frame, height=1, width= 10, fg='red', bg='darkorchid4', text= "Attempt " + str(attempt), font= ('Cambria', 24, 'bold'))
 			att.grid(row=0, column=2, sticky = tk.NSEW)
-		elif attempt == 6:
-			if distance > prev_distance:
-				score = tk.Label(t2_frame, height=1, width= 44, fg='white', bg='darkorchid4', text= "Best Score= " + str(prev_distance) + "% away from the colour", font= ('Verdana', '12'))
-				score.grid(row=0, column=2, sticky = tk.NSEW)
-			elif distance <= prev_distance:
-				score = tk.Label(t2_frame, height=1, width= 44, fg='white', bg='darkorchid4', text= "Best Score= " + str(distance) + "% away from the colour", font= ('Verdana', '12'))
-				score.grid(row=0, column=2, sticky = tk.NSEW)
-			att = tk.Label(at_frame, height=1, width= 10, fg='red', bg='darkorchid4', text= " Attempt 5 ", font= ('Cambria', 24, 'bold'))
-			att.grid(row=0, column=2, sticky = tk.NSEW)
 		if distance >=10.0 and attempt == 6: # Αν δε τα καταφερει
+			att = tk.Label(at_frame, height=1, width= 10, fg='red', bg='darkorchid4', text= "Attempt 5", font= ('Cambria', 24, 'bold'))
+			att.grid(row=0, column=2, sticky = tk.NSEW)
 			tk.messagebox.showinfo(" ", "YOU LOSE !")
 			rst = True
-		if distance <= 10.0 and attempt == 2: # Αν τα καταφέρει με τη πρώτη
+		if distance <= 10.0 and attempt == 2: # Αν τα καταφέρει με τη πρώτη προσπάθεια
+			att = tk.Label(at_frame, height=1, width= 10, fg='red', bg='darkorchid4', text= "Attempt " + str(attempt-1), font= ('Cambria', 24, 'bold'))
+			att.grid(row=0, column=2, sticky = tk.NSEW)
 			tk.messagebox.showinfo(" ", "YOU'RE SKILLED ;D")
 			rst = True
 		elif distance <= 10.0 and (attempt <= 5 and attempt >= 3):
+			att = tk.Label(at_frame, height=1, width= 10, fg='red', bg='darkorchid4', text= "Attempt " + str(attempt-1), font= ('Cambria', 24, 'bold'))
+			att.grid(row=0, column=2, sticky = tk.NSEW)
 			tk.messagebox.showinfo(" ", "NOT BAD !")
 			rst = True
-		elif distance <= 10.0 and attempt == 6: # Αν τα καταφέρει με τη τελευταία
+		elif distance <= 10.0 and attempt == 6: # Αν τα καταφέρει με τη τελευταία προσπάθεια
+			att = tk.Label(at_frame, height=1, width= 10, fg='red', bg='darkorchid4', text= "Attempt 5", font= ('Cambria', 24, 'bold'))
+			att.grid(row=0, column=2, sticky = tk.NSEW)
 			tk.messagebox.showinfo(" ", "YOU WIN!")
 			rst = True
 	elif rst == True:
@@ -99,7 +115,7 @@ def di():
 		att.grid(row=0, column=2, sticky = tk.NSEW)
 		score = tk.Label(t2_frame, height=1, width= 30, fg='white', bg='darkorchid4', text= " ")
 		score.grid(row=0, column=2, sticky = tk.NSEW)
-		tk.messagebox.showinfo(" ", "PRESS 'GENERATE' TO PLAY AGAIN!")
+		tk.messagebox.showwarning(" ", "PRESS 'GENERATE' TO PLAY AGAIN!")
 
 
 if __name__ == '__main__':
@@ -115,10 +131,14 @@ if __name__ == '__main__':
 	green.grid(row=20, column=0, padx=10, pady=20)
 	blue = tk.Scale(root,from_=0,to=255,fg='white', bg='dark blue', sliderlength= 15, orient='horizontal', command= upd)
 	blue.grid(row=30, column=0,padx=10,pady=20)
-	button1 = tk.Button(root, text='Submit', fg= 'cyan', bg= 'darkorchid4', command = di) # Κώδικας για τη δημιουργία κουμπιού το οποιο θα κάνει submit το χρώμα του παίκτη
+	button1 = tk.Button(root, background= 'cyan', foreground= 'darkorchid4',  relief='flat', text= '  Submit  ', cursor= 'hand2', activebackground= 'white', activeforeground= 'cyan', font=('Verdana', 9), command = di) # Κώδικας για τη δημιουργία κουμπιού το οποιο θα κάνει submit το χρώμα του παίκτη
 	button1.grid(row=0, column=0, padx=0, pady=0) 
-	button2 = tk.Button(root, text='Generate', fg= 'cyan', bg= 'darkorchid4', command= random_col) # Κώδικας για τη δημιουργία κουμπιού που θα κάνει generate νέο χρώμα
-	button2.grid(row=5, column=0, padx=0, pady=0)
+	button1.bind('<Enter>', cursorb1)
+	button1.bind('<Leave>', no_cursorb1)
+	button2 = tk.Button(root, background= 'cyan', foreground= 'darkorchid4', relief= 'flat', text='Generate', cursor= 'hand2', activebackground= 'white', activeforeground= 'cyan', font=('Verdana', 9), command= random_col) # Κώδικας για τη δημιουργία κουμπιού που θα κάνει generate νέο χρώμα
+	button2.grid(row=8, column=0, padx=0, pady=0)
+	button2.bind('<Enter>', cursorb2)
+	button2.bind('<Leave>', no_cursorb2)
 	pl_frame = tk.Frame(root, bg="black", width=450, height=270) # Φτιάχνει ένα πλαίσιο που θα έχει default τιμή το μαύρο και θα αλλάζει ανάλογα με τις επιλογές του χρήστη
 	pl_frame.grid(row=50, column=0, padx=0, pady=10)
 	ai_frame = tk.Frame(root, bg="black", width=450, height=270)
